0b014352caad3662a7ae613cd007c42896f1326a
SleekPanther <macrobyte2@gmail.com>
2018-03-14 18:16:45 -0400

    Fxml setup

---
+++ Sorry.java
@@ -0,0 +1,30 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.*;
+import javafx.scene.input.*;
+import javafx.stage.*;
+
+public class Sorry extends Application {
+	private static final String uiFileName = "view.fxml";
+
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+		Parent root = FXMLLoader.load(getClass().getResource(uiFileName));
+		primaryStage.setTitle("Sorry");
+		primaryStage.setResizable(false);
+		primaryStage.setScene(new Scene(root));
+
+		primaryStage.addEventHandler(KeyEvent.KEY_PRESSED, (KeyEvent event) -> {	//Destroy on key PRESS ESC
+			if (KeyCode.ESCAPE == event.getCode()) {
+				primaryStage.close();
+			}
+		});
+		
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
---
+++ SorryController.java
@@ -0,0 +1,28 @@
+import java.net.URL;
+import javafx.fxml.*;
+import javafx.application.Platform;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.*;
+import java.util.*;
+import javafx.collections.*;
+import java.text.DecimalFormat;
+
+public class SorryController implements Initializable {
+
+
+	public SorryController(){
+	}
+	
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+	}
+
+
+}
