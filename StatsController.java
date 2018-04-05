6f7305979b806d65deeeebe66b48e06f2fdfe72c
Michelle A Niland <michelle.niland@uvm.edu>
2018-04-05 16:27:28 -0400

    added database connection code

--- StatsController.java
+++ StatsController.java
@@ -10,6 +10,10 @@ import javafx.scene.shape.Circle;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.*;
 import javafx.collections.*;
 import java.text.DecimalFormat;
@@ -20,14 +24,31 @@ public class StatsController extends BaseController implements Initializable{
 	private Scene menuScene;
 	
 	@FXML private Button mainMenuButton;
+	@FXML private Button dbButton;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		mainMenuButton.setOnAction((event) -> changeScene(menuScene, event));
+		dbButton.setOnAction((event) -> connectToDatabase());
+
 	}
 	
 	public void setMenuScene(Scene scene) {
 		menuScene = scene;
+	}
+
+	public void connectToDatabase(){
+		String url = "jdbc:mysql://localhost:3306/sorrygame";
+		String username = "root";
+		String password = "password";
+
+		System.out.println("Connecting database...");
+
+		try (Connection connection = DriverManager.getConnection(url, username, password)) {
+			System.out.println("Database connected!");
+		} catch (SQLException e) {
+			throw new IllegalStateException("Cannot connect the database!", e);
+		}
 	}
 
 }
