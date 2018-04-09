f88c6c17e0307a8fd272863186ec3c095ca6a931
Michelle A Niland <michelle.niland@uvm.edu>
2018-04-09 12:35:56 -0400

    working on getting database data to show in table stats page

--- StatsController.java
+++ StatsController.java
@@ -13,6 +13,7 @@ import javafx.event.*;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.*;
 import javafx.collections.*;
@@ -22,15 +23,17 @@ import javafx.scene.input.MouseEvent;
 
 public class StatsController extends BaseController implements Initializable{
 	private Scene menuScene;
+
 	
 	@FXML private Button mainMenuButton;
 	@FXML private Button dbButton;
+	@FXML private TableView tableView;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		mainMenuButton.setOnAction((event) -> changeScene(menuScene, event));
 		dbButton.setOnAction((event) -> connectToDatabase());
-
+		buildTable();
 	}
 	
 	public void setMenuScene(Scene scene) {
@@ -49,6 +52,37 @@ public class StatsController extends BaseController implements Initializable{
 		} catch (SQLException e) {
 			throw new IllegalStateException("Cannot connect the database!", e);
 		}
+	}
+
+	public void buildTable(){
+		String url = "jdbc:mysql://localhost:3306/sorrygame";
+		String username = "root";
+		String password = "password";
+
+		try (Connection connection = DriverManager.getConnection(url, username, password)) {
+			System.out.println("Database connected!");
+			String SQL = "SELECT * from tblsorrygame";
+			ResultSet rs = connection.createStatement().executeQuery(SQL);
+
+			ObservableList<ObservableList> statsData=FXCollections.observableArrayList();
+			while(rs.next()){
+				//Iterate Row
+				ObservableList<String> row = FXCollections.observableArrayList();
+				for(int i=1 ; i<=rs.getMetaData().getColumnCount(); i++){
+					//Iterate Column
+					row.add(rs.getString(i));
+				}
+				System.out.println("Row [1] added "+row );
+				statsData.add(row);
+
+
+			}
+			tableView.setItems(statsData);
+		} catch (SQLException e) {
+			throw new IllegalStateException("Cannot connect the database!", e);
+		}
+
+
 	}
 
 }
