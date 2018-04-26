7842bb8bea3ec2b17ebb7c482ad6454591296341
SleekPanther <macrobyte2@gmail.com>
2018-04-26 07:54:56 -0400

    Menu controller move some things to ColorFunctions

--- Functions/ColorFunctions.java
+++ Functions/ColorFunctions.java
@@ -1,13 +1,14 @@
 package Functions;
 import enums.Color;
 
+//Convert between integers representing players and their colors
 public class ColorFunctions {
 	public static int colorToPlayerIndex(Color color){
 		return colorToPlayerIndex(color.name());
 	}
 	
 	public static int colorToPlayerIndex(String color){
-		color = color.toUpperCase();
+		color = color.toUpperCase().trim();
 		if(color=="RED"){
 			return 0;
 		}
@@ -21,6 +22,23 @@ public class ColorFunctions {
 			return 3;
 		}
 		return -1;
+	}
+
+	public static Color stringToColor(String stringColor){
+		stringColor = stringColor.toUpperCase().trim();
+		if(stringColor.equals("RED")){
+			return Color.RED;
+		}
+		else if(stringColor.equals("BLUE")){
+			return Color.BLUE;
+		}
+		else if(stringColor.equals("YELLOW")){
+			return Color.YELLOW;
+		}
+		else if(stringColor.equals("GREEN")){
+			return Color.GREEN;
+		}
+		return Color.ANY;
 	}
 
 }
